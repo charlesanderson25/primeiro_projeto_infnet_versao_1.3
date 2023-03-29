14dad2e1c1633a312bd2af8b6dfdf90f72e14318
charlesanderson25 <charles_spfc@hotmail.com>
2023-03-29 20:09:52 -0300

    Substituição do evento onClick por addEventListener

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
-function redirectToHome(){
-    window.location.href = "index.html";
-}
+// function redirectToHome(){
+//     window.location.href = "index.html";
+// }
+
+const direcionaParaHome = document.querySelector('.portfolio');
+
+direcionaParaHome.addEventListener('click', redireciona => {
+    window.location.href="index.html";
+})
